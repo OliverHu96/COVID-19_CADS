73d655832a48de15331d31e309d3448c5433d984
OliverHu96 <73470185+OliverHu96@users.noreply.github.com>
2022-12-01 20:21:57 +0800

    Add files via upload

---
+++ COVID-19_CADS/COVID-19_CADS/models/CA.py
@@ -0,0 +1,168 @@
+import torch
+import torch.nn as nn
+import math
+import torch.utils.model_zoo as model_zoo
+
+
+class h_sigmoid(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_sigmoid, self).__init__()
+        self.relu = nn.ReLU6(inplace=inplace)
+
+    def forward(self, x):
+        return self.relu(x + 3) / 6
+
+
+class h_swish(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_swish, self).__init__()
+        self.sigmoid = h_sigmoid(inplace=inplace)
+
+    def forward(self, x):
+        return x * self.sigmoid(x)
+
+
+class CoordAttention(nn.Module):
+
+    def __init__(self, in_channels, out_channels, reduction=32):
+        super(CoordAttention, self).__init__()
+        self.pool_w = nn.AdaptiveAvgPool2d((1, None))
+        self.pool_h = nn.AdaptiveAvgPool2d((None, 1))
+        temp_c = max(8, in_channels // reduction)
+
+        self.conv1 = nn.Conv2d(in_channels, temp_c, kernel_size=1, stride=1, padding=0)
+        self.bn1 = nn.BatchNorm2d(temp_c)
+        self.conv2 = nn.Conv2d(temp_c, out_channels, kernel_size=1, stride=1, padding=0)
+        self.conv3 = nn.Conv2d(temp_c, out_channels, kernel_size=1, stride=1, padding=0)
+        self.relu = h_swish()
+
+    def forward(self, x):
+        identity = x
+        n, c, H, W = x.size()
+        x_h = self.pool_h(x)
+        x_w = self.pool_w(x).permute(0, 1, 3, 2)
+        out = torch.cat([x_h, x_w], dim=2)
+        out = self.conv1(out)
+        out = self.bn1(out)
+        out = self.relu(out)
+        x_h, x_w = torch.split(out, [H, W], dim=2)
+        x_w = x_w.permute(0, 1, 3, 2)
+
+        out_h = torch.sigmoid(self.conv2(x_h))
+        out_w = torch.sigmoid(self.conv3(x_w))
+        return identity * out_w * out_h
+
+#残差块
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
+                               padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * 4)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.ca = CoordAttention(in_channels=planes * self.expansion, out_channels=planes * self.expansion)
+
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        out = self.ca(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+class ResNet(nn.Module):
+
+    def __init__(self, block, layers, num_classes=1000):
+        self.inplanes = 64
+        super(ResNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        self.classifier = nn.Linear(512 * block.expansion, num_classes)
+
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = x.view(-1, 2048)
+        x = self.classifier(x)
+
+        return x
+    
+def resnet50(pretrained=False, num_classes=2, **kwargs):
+    model = ResNet(Bottleneck, [3, 4, 6, 3], num_classes, **kwargs)
+    if pretrained:
+        pretrained_model = model_zoo.load_url('https://download.pytorch.org/models/resnet50-19c8e357.pth')
+        state = model.state_dict()
+        for key in state.keys():
+            if key in pretrained_model.keys():
+                state[key] = pretrained_model[key]
+        model.load_state_dict(state)
+    return model
+ 
---
+++ COVID-19_CADS/COVID-19_CADS/models/resFPNCBAM.py
@@ -0,0 +1,231 @@
+import torch
+import torch.nn as nn
+import torch.utils.model_zoo as model_zoo
+import math
+import torch.nn.functional as F
+
+class ChannelAttention(nn.Module):
+    def __init__(self, in_planes, ratio=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+
+        self.fc = nn.Sequential(nn.Conv2d(in_planes, in_planes // 16, 1, bias=False),
+                                nn.ReLU(),
+                                nn.Conv2d(in_planes // 16, in_planes, 1, bias=False))
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = self.fc(self.avg_pool(x))
+        max_out = self.fc(self.max_pool(x))
+        out = avg_out + max_out
+        return self.sigmoid(out)
+
+
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+
+        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=kernel_size // 2, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv1(x)
+        return self.sigmoid(x)
+    
+# backbone network
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
+                               padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * 4)
+        self.relu = nn.ReLU(inplace=True)
+        
+        self.ca = ChannelAttention(planes * 4)
+        self.sa = SpatialAttention()
+        
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+        
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+        
+        out = self.ca(out) * out
+        out = self.sa(out) * out
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet(nn.Module):
+
+    def __init__(self, block, layers, num_classes=7, scale=1):
+        self.inplanes = 64
+        super(ResNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+
+        # Top layer
+        self.toplayer = nn.Conv2d(2048, 256, kernel_size=1, stride=1, padding=0)  # Reduce channels
+        self.toplayer_bn = nn.BatchNorm2d(256)
+        self.toplayer_relu = nn.ReLU(inplace=True)
+
+        # Smooth layers
+        self.smooth1 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.smooth1_bn = nn.BatchNorm2d(256)
+        self.smooth1_relu = nn.ReLU(inplace=True)
+
+        self.smooth2 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.smooth2_bn = nn.BatchNorm2d(256)
+        self.smooth2_relu = nn.ReLU(inplace=True)
+
+        self.smooth3 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.smooth3_bn = nn.BatchNorm2d(256)
+        self.smooth3_relu = nn.ReLU(inplace=True)
+
+        # Lateral layers
+        self.latlayer1 = nn.Conv2d(1024, 256, kernel_size=1, stride=1, padding=0)
+        self.latlayer1_bn = nn.BatchNorm2d(256)
+        self.latlayer1_relu = nn.ReLU(inplace=True)
+
+        self.latlayer2 = nn.Conv2d(512, 256, kernel_size=1, stride=1, padding=0)
+        self.latlayer2_bn = nn.BatchNorm2d(256)
+        self.latlayer2_relu = nn.ReLU(inplace=True)
+
+        self.latlayer3 = nn.Conv2d(256, 256, kernel_size=1, stride=1, padding=0)
+        self.latlayer3_bn = nn.BatchNorm2d(256)
+        self.latlayer3_relu = nn.ReLU(inplace=True)
+        
+        self.conv2 = nn.Conv2d(1024, 256, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(256)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.conv3 = nn.Conv2d(256, num_classes, kernel_size=1, stride=1, padding=0)
+        self.scale = scale
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def _upsample(self, x, y, scale=1):
+        _, _, H, W = y.size()
+        return F.upsample(x, size=(H // scale, W // scale), mode='bilinear')
+
+    def _upsample_add(self, x, y):
+        _, _, H, W = y.size()
+        return F.upsample(x, size=(H, W), mode='bilinear') + y
+
+    def forward(self, x):
+        h = x
+        h = self.conv1(h)
+        h = self.bn1(h)
+        h = self.relu1(h)
+        h = self.maxpool(h)
+
+        h = self.layer1(h)
+        c2 = h
+        h = self.layer2(h)
+        c3 = h
+        h = self.layer3(h)
+        c4 = h
+        h = self.layer4(h)
+        c5 = h
+
+        # Top-down
+        p5 = self.toplayer(c5)
+        p5 = self.toplayer_relu(self.toplayer_bn(p5))
+
+        c4 = self.latlayer1(c4)
+        c4 = self.latlayer1_relu(self.latlayer1_bn(c4))
+        p4 = self._upsample_add(p5, c4)
+        p4 = self.smooth1(p4)
+        p4 = self.smooth1_relu(self.smooth1_bn(p4))
+
+        c3 = self.latlayer2(c3)
+        c3 = self.latlayer2_relu(self.latlayer2_bn(c3))
+        p3 = self._upsample_add(p4, c3)
+        p3 = self.smooth2(p3)
+        p3 = self.smooth2_relu(self.smooth2_bn(p3))
+
+        c2 = self.latlayer3(c2)
+        c2 = self.latlayer3_relu(self.latlayer3_bn(c2))
+        p2 = self._upsample_add(p3, c2)
+        p2 = self.smooth3(p2)
+        p2 = self.smooth3_relu(self.smooth3_bn(p2))
+
+        p3 = self._upsample(p3, p2)
+        p4 = self._upsample(p4, p2)
+        p5 = self._upsample(p5, p2)
+
+        out = torch.cat((p2, p3, p4, p5), 1)
+        out = self.conv2(out)
+        out = self.relu2(self.bn2(out))
+        out = self.conv3(out)
+
+        return out
+
+def resnet50(pretrained=False, **kwargs):
+    model = ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
+    if pretrained:
+        pretrained_model = model_zoo.load_url('https://download.pytorch.org/models/resnet50-19c8e357.pth')
+        state = model.state_dict()
+        for key in state.keys():
+            if key in pretrained_model.keys():
+                state[key] = pretrained_model[key]
+        model.load_state_dict(state)
+    return model
